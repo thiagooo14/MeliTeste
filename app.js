c941eeb00ec028b85f0f69ce9a5ff82eee0a4c6e
thiagooo14 <thiago.stavares@mercadolivre.com>
2022-07-26 00:38:58 -0300

    refatorando

--- app.js
+++ app.js
@@ -1,50 +1,42 @@
 const express = require('express');
-const products = require('./products');
+const port = 3000;
 const app = express();
+
+const products = require('./products');
+
 app.use(express.json());
 
-const port = 3000;
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World!');
+});
 
-app.get('/produtcs', (req, res) => {
+app.get('/products', (req, res) => {
   res.status(200).json(products);
 });
 
-app.get('/produtcs/:id', (req, res) => {
-  const { id } = req.params;
-  const product = products.find((product) => product.id === id);
-  res.status(200).json(product);
-});
-
-app.post('/produtcs', (req, res) => {
-  const content = req.body;
-  const newContent = [...products, content];
-  res.status(201).json(newContent);
-});
-
-app.post('/produtcs/:id', (req, res) => {
-  const content = req.body;
+app.get('/products/:id', (req, res) => {
   const id = Number(req.params.id);
+  const productID = products.findIndex((item) => item.id === id);
+  res.json(products[productID]);
+});
 
-  const product = products.find((product) => product.id === id);
+app.post('/products', (req, res) => {
+  products.push(req.body);
+  res.status(201).send(`produto ${req.body.name} criado`);
+});
 
-  const updatedProducts = products.map((product) => {
-    if (product.id === id) {
-      return content;
-    }
-    return product;
-  });
-
-  products = updatedProducts;
-
-  res.status(201).send(product);
+app.put('/products/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const productID = products.findIndex((item) => item.id === id);
+  products[productID] = req.body;
+  res.json(products);
 });
 
 app.delete('/products/:id', (req, res) => {
   const id = Number(req.params.id);
-  const withoutDeletedProduct = products.filter((product) => product.id !== id);
-
-  products = withoutDeletedProduct;
-  res.status(200).json(products);
+  const productID = products.findIndex((item) => item.id === id);
+  products.splice(productID, 1);
+  res.json(`produto ${id} excluido`);
 });
 
 app.listen(port, () => {
