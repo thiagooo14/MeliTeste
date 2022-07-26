5191d47ff5b2abba9b93b6a77726bc036e76a682
thiagooo14 <thiago.stavares@mercadolivre.com>
2022-07-26 01:10:25 -0300

    express-routes

--- app.js
+++ app.js
@@ -1,42 +1,18 @@
 const express = require('express');
+const productRoutes = require('/routes/productRoutes.js');
 const port = 3000;
 const app = express();
 
-const products = require('./products');
+const productRoutes = require('./routes/productRoutes');
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello World!');
-});
+app.use('/produtc', productRoutes);
 
-app.get('/products', (req, res) => {
-  res.status(200).json(products);
-});
+app.use('*', (req, res, next) => {
+  res.status(404).send('error 404, not found');
 
-app.get('/products/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const productID = products.findIndex((item) => item.id === id);
-  res.json(products[productID]);
-});
-
-app.post('/products', (req, res) => {
-  products.push(req.body);
-  res.status(201).send(`produto ${req.body.name} criado`);
-});
-
-app.put('/products/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const productID = products.findIndex((item) => item.id === id);
-  products[productID] = req.body;
-  res.json(products);
-});
-
-app.delete('/products/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const productID = products.findIndex((item) => item.id === id);
-  products.splice(productID, 1);
-  res.json(`produto ${id} excluido`);
+  next();
 });
 
 app.listen(port, () => {
